53ba02a3eaa8346b88a29ef4c40740d55a829361
Sam Biggs <eeysb11@gmail.com>
2021-10-31 09:55:32 +0000

    Add project files.

---
+++ KatiesGarden.Web/Client/Pages/Index.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KatiesGarden.Web.Client.Pages
+{
+    
+    public partial class Index : ComponentBase
+    {
+        List<string> imageNames;
+        int numberOfImages;
+
+        public Index()
+        {
+            imageNames = new List<string>
+            {
+                "IMG_0775.JPG",
+                "IMG_0783.JPG",
+                //"IMG_0785.JPG",
+                "IMG_0788.JPG",
+                "IMG_0792.JPG",
+                "IMG_0795.JPG",
+                "IMG_0798.JPG",
+                "IMG_0801.JPG",
+                "IMG_0805.JPG",
+                "IMG_0808.JPG",
+                "IMG_0815.JPG",
+                "IMG_0827.JPG",
+                "IMG_0830.JPG",
+                "IMG_0834.JPG",
+                "IMG_0835.JPG",
+                "IMG_0839.JPG",
+                "IMG_0841.JPG",
+                "IMG_0851.JPG",
+            };
+            numberOfImages = imageNames.Count();
+        }
+
+
+    }
+}
---
+++ KatiesGarden.Web/Server/Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KatiesGarden.Web.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet("/{filePath}")]        
+        public IActionResult GetImage(string filePath, int type)
+        {            
+            var contentType = type == 1 ? "image/jpeg" : "video/mp4";
+
+            //var filePath = Path.Combine(_env.ContentRootPath, "Images", $"{filename}");
+
+            if (Directory.Exists("folderPath"))
+            {
+                var image = System.IO.File.OpenRead(filePath);
+                return File(image, "image/jpeg");
+            }
+
+            return new NotFoundObjectResult("Image at file path: '" + filePath + " was not found");
+        }        
+    }
+}
