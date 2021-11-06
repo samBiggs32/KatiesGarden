8212958a4646b016eb5e81e1c0622cafa84ec461
Sam Biggs <eeysb11@gmail.com>
2021-11-06 10:01:04 +0000

    index_page (finally) implemented map on contact page

---
+++ KatiesGarden.Web/Client/wwwroot/scripts/scripts.js
@@ -0,0 +1,9 @@
+﻿var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: { lat: 51.0233371428786, lng: - 3.2553684238706695},        
+        mapTypeId: "terrain",
+    });  
+}
