0ca5acfb1718727193e5aa825abae00a49091a19
Sam Biggs <eeysb11@gmail.com>
2021-11-28 16:33:18 +0000

    clean up

--- KatiesGarden.Web/Client/Models/Images.cs
+++ KatiesGarden.Web/Client/Models/Images.cs
@@ -8,7 +8,7 @@
                 "IMG_0796.JPG",
                 "IMG_0808.JPG",
                 "IMG_0848.JPG",
-                "IMG_0852.JPG",                
+                "IMG_0852.JPG",
             };
     }
 }
